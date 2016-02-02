6f5202021cd1b6e6ada695e42e9f486a5896bd42
Tammer Galal <tg385@scarletmail.rutgers.edu>
2016-02-01 20:44:29 -0500

    changed pass

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : 'tinker511',
+  password : '',
   database : 'zoo_db'
 });
 //prompt
