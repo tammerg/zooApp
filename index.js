5a8ffce7522c28515df0cd87079ca4ddaab01905
Tammerg <tammergalal20@gmail.com>
2016-02-17 19:14:51 -0500

    fixing ertror in defaulting to 'didnt get that'

--- index.js
+++ index.js
@@ -1,166 +1,177 @@
 //mysql connection
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'zoo_db'
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'zoo_db'
 });
 //prompt
 var prompt = require('prompt');
 prompt.start();
 var zoo = {
-  welcome : function (){
+  welcome: function() {
     console.log("welcome to the zoo and friends app~");
   },
-  menu : function (){
+  menu: function() {
     console.log("Enter (A): ------> to Add a new animal to the Zoo!");
     console.log("Enter (U): ------> to Update info on an animal in the Zoo!");
     console.log("Enter (V): ------> to Visit the animals in the Zoo!");
     console.log("Enter (D): ------> to Adopt an animal from the Zoo!");
     console.log("Enter (Q): ------> to Quit and exit the Zoo!");
   },
-   add : function(input_scope){
+  add: function(input_scope) {
     var currentScope = input_scope;
     var query = "INSERT INTO zoo_db(name, type, age) VALUES (?,?,?)";
     console.log("To add an animal to the zoo please fill out the following form for us!");
-    prompt.get(["->", "name", "type", "age"], function(err, result){
-        connection.query(query);
+    prompt.get(["->", "name", "type", "age"], function(err, result) {
+      connection.query(query);
     });
     currentScope.menu();
     currentScope.promptUser();
   },
-  visit : function(){
+  visit: function() {
     console.log(" Enter (I): ------> do you know the animal by it's id? We will visit that animal!");
     console.log(" Enter (N): ------> do you know the animal by it's name? We will visit that animal!");
     console.log(" Enter (A): ------> here's the count for all animals in all locations!");
-    console.log(" Enter (C): ------> here's the count for all animals in this one city!");   
+    console.log(" Enter (C): ------> here's the count for all animals in this one city!");
     console.log(" Enter (O): ------> here's the count for all the animals in all locations by the type you specified!");
     console.log(" Enter (Q): ------> Quits to the main menu!");
     currentScope.visit();
     currentScope.view(currentScope);
   },
-  view : function(){
+  view: function(input_scope) {
     var currentScope = input_scope;
     console.log("Please choose where you would like to visit");
-    prompt.get(["->", "visit"], function(err, result){
-      if (result.visit == "Q"){
+    prompt.get(["->", "visit"], function(err, result) {
+      if (result.visit == "Q") {
         currentScope.menu();
-      }else if (result.visit == "O"){
+      } else if (result.visit == "O") {
         currentScope.type(input_scope);
-      }else if (result.type == "I"){
+      } else if (result.type == "I") {
         currentScope.type(input_scope);
-      }else if (result.animID == "N"){
+      } else if (result.animID == "N") {
         currentScope.name(input_scope);
-      }else if (result.name == "A"){
+      } else if (result.name == "A") {
         currentScope.all(input_scope);
-      }else if (result.all == "C"){
+      } else if (result.all == "C") {
         currentScope.care(input_scope);
-      }else{
+      } else {
         console.log("Sorry, didn't get that.  Come again?");
-          currentScope.visit();
-          currentScope.view(currentScope);
-    }
+        currentScope.visit();
+        currentScope.view(currentScope);
+      }
     });
   },
-  type : function(input_scope){
+  type: function(input_scope) {
     var currentScope = input_scope;
     console.log("Enter an animal to see how many we have!");
-    prompt.get(["->", "animal_type"], function(err, result){
+    prompt.get(["->", "animal_type"], function(err, result) {
       connection.query();
       currentScope.menu();
       currentScope.prompUser();
     });
   },
-  care : function(input_scope){
-   var currentScope = input_scope;
-   console.log("Enter City Name: NY/SF");
-   prompt.get(["->", "city_name"], function(err, result){
-    connection.query();
-    currentScope.visit();
-    currentScope.view(currentScope);
-   });
+  care: function(input_scope) {
+    var currentScope = input_scope;
+    console.log("Enter City Name: NY/SF");
+    prompt.get(["->", "city_name"], function(err, result) {
+      connection.query();
+      currentScope.visit();
+      currentScope.view(currentScope);
+    });
   },
-  animId : function (input_scope){
+  animId: function(input_scope) {
     var currentScope = input_scope;
     console.log("Enter ID of the animal you wish to visit");
-    prompt.get(["->", "animal_id"], function(err, result){
+    prompt.get(["->", "animal_id"], function(err, result) {
       connection.query();
       currentScope.menu();
       currentScope.promptUser();
     });
   },
-  anim_name : function (input_scope){
+  anim_name: function(input_scope) {
     var currentScope = input_scope;
     console.log("Enter the Name of the animal you wish to visit");
-    prompt.get(["->", "animal_name"], function(err, result){
+    prompt.get(["->", "animal_name"], function(err, result) {
       connection.query();
       currentScope.menu();
       currentScope.promptUser();
     });
   },
-  count : function(input_scope){
+  count: function(input_scope) {
     var currentScope = input_scope;
     console.log("Type all to see the current total count of animals in our zoo!");
-    prompt.get(["->", "animal_count"], function(err, result){
+    prompt.get(["->", "animal_count"], function(err, result) {
       connection.query();
       currentScope.menu();
       currentScope.prompUser();
     });
   },
-  update : function(input_scope){
+  all: function(input_scope) {
+    this.currentScope = input_scope;
+    console.log("Wanna see how many animals there are total?")
+    prompt.get(['yes'], function(err, result) {
+      if (err) throw err;
+      connection.query('SELECT * FROM animals', function(err, results) {
+        if (err) {
+          console.log(err)
+        };
+        console.log("there are this many animals in the zoo: " + results.length)
+        zoo.menu();
+        zoo.promptUser();
+      })
+    })
+  },
+  update: function(input_scope) {
     var currentScope = input_scope;
-    prompt.get(["--->", "new_name", "new_age", "new_type", "new_caretaker_id"], function(err, result){
+    prompt.get(["--->", "new_name", "new_age", "new_type", "new_caretaker_id"], function(err, result) {
       connection.query();
       currentScope.menu();
       currentScope.prompUser();
     });
   },
-  adopt : function(){
+  adopt: function() {
     var currentScope = input_scope;
-    prompt.get(["->", "animal_id"], function(err, result){
-      if (result.input == "Q"){
+    prompt.get(["->", "animal_id"], function(err, result) {
+      if (result.input == "Q") {
         self.exit();
-      }else if (result.input == "A"){
+      } else if (result.input == "A") {
         self.add(self);
-      }else if (result.input == "V"){
+      } else if (result.input == "V") {
         self.visit();
         self.view(self);
-      }else if (result.input == "D"){
+      } else if (result.input == "D") {
         self.adopt(self);
-      }else{
+      } else {
         console.log("Sorry, didn't get that. Try again");
         self.promptUser();
         self.menu();
       }
     });
   },
-  promptUser : function(){
+  promptUser: function() {
     var self = this;
-    prompt.get("input", function(err, result){
-      if(result.input == "Q"){
+    prompt.get("input", function(err, result) {
+      if (result.input == "Q") {
         self.exit();
-      }
-      else if(result.input == "A"){
+      } else if (result.input == "A") {
         self.add(self);
-      }
-      else if(result.input == "V"){
+      } else if (result.input == "V") {
         self.visit();
         self.view(self);
-      }
-      else if(result.input == "D"){
+      } else if (result.input == "D") {
         self.adopt(self);
-      }
-      else{
+      } else {
         console.log("Sorry, didn't get that.  Come again?")
       }
     })
   },
-  exit : function(){
+  exit: function() {
     console.log("Thank you for visiting Tammer's Zoo of Horrors, Bye!");
     process.exit();
   },
-  open : function(){
+  open: function() {
     this.welcome();
     this.menu();
     this.promptUser();
